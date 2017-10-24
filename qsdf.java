615dac95b50a46784bc881220b50162351510c6b
Brayan <brayan.delmee@student.uclouvain.be>
2017-10-24 17:54:02 +0200

    m

--- qsdf.java
+++ qsdf.java
@@ -1,33 +1,4 @@
+j'aime ton boule jérome!!!!
 
-/**
- * Write a description of class qsdf here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class qsdf
-{
-    // instance variables - replace the example below with your own
-    private int x;
 
-    /**
-     * Constructor for objects of class qsdf
-     */
-    public qsdf()
-    {
-        // initialise instance variables
-        x = 0;
-    }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-}
